a30de92052f91068307a01094c9e5e57e3cbc801
geykeranna <62444758+geykeranna@users.noreply.github.com>
2021-03-14 17:44:06 +0300

    feat: add function body

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = [];
+    if (matrix === undefined || matrix.length === 0) return arr ;
+    for (let i = 0; i < matrix.length; i++)
+    {
+        if (i % 2 === 0) {
+            for (let j = 0; j <  matrix[i].length; j++) {
+                arr.push(matrix[i][j]);
+            }
+        } else {
+            for (let k = matrix[i].length - 1; k >= 0; k--) {
+                arr.push(matrix[i][k]);
+            }
+        }
+    }
+    return arr;
 }
